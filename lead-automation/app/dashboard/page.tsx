f2ad99779a8e9995d758a68926d706d2ca159152
Deepanshu-Z <deepanshupokhriyal07@gmail.com>
2026-04-28 17:45:40 +0530

    updated

--- lead-automation/app/dashboard/page.tsx
+++ lead-automation/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { useEffect } from "react";
+
 export default function page() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`/api/leads`);
+      console.log(res);
+    };
+    fetchData();
+  }, []);
   return <div>page</div>;
 }
